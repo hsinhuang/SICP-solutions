f4bd0e41e81f40858577146b27738386acd3163c
leon-huang <leon@njuopen.com>
2013-02-27 21:57:14 +0800

    ex2-12

---
+++ chap2/12.scm
@@ -0,0 +1,20 @@
+#lang scheme
+
+(provide (all-defined-out))
+(require "11.scm")
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (make-center-width c (* c p)))
+
+(define (percent i)
+  (/ (width i)
+     (center i)))
