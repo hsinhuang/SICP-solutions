b22e6a5db4f9320a00aa6fceb5233e16f2ee19a5
leon-huang <leon@njuopen.com>
2013-02-27 19:50:18 +0800

    ex2-4

---
+++ chap2/4.scm
@@ -0,0 +1,13 @@
+#lang scheme
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 3 5))
+(cdr (cons 4 -1))
