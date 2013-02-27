c0e793672320e5fcf3ba3aae1b47f66c1be940fc
leon-huang <leon@njuopen.com>
2013-02-27 19:15:42 +0800

    ex2-2

---
+++ chap2/2.scm
@@ -0,0 +1,36 @@
+#lang scheme
+
+(provide (all-defined-out))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment p1 p2)
+  (cons p1 p2))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (midpoint-segment s)
+  (define (average x1 x2) (/ (+ x1 x2) 2.0))
+  (make-point (average (x-point (start-segment s))
+                       (x-point (end-segment s)))
+              (average (y-point (start-segment s))
+                       (y-point (end-segment s)))))
