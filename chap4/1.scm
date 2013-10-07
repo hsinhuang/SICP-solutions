27d1fb2b63a95f342e5f6e23a05c0f166f30bf11
leon-huang <leon@njuopen.com>
2013-10-07 19:33:01 +0800

    add a right to left eval version to 4.1

--- chap4/1.scm
+++ chap4/1.scm
@@ -1,8 +1,17 @@
 #lang scheme
 
+;; eval left to right
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
       (let ([first (eval (first-operand exps) env)]
             [rest (list-of-values (rest-operands exps) env)])
-        (cons first rest)))
+        (cons first rest))))
+
+;; eval right to left
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ([rest (list-of-values (rest-operands exps) env)]
+            [first (eval (first-operand exps) env)])
+        (cons first rest))))
